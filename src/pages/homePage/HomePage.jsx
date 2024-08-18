836f4a837b95785c023802556a5e075648cb3d29
Sakrid1s <bilinets.vitalik@gmail.com>
2024-08-18 23:09:16 +0300

    upd

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,10 +1,24 @@
 import DocumentTitle from '../../components/documentTitle/DocumentTitle';
+import css from './HomePage.module.css';
 
 const HomePage = () => {
   return (
-    <>
+    <section className={css.homeSection}>
       <DocumentTitle>Home</DocumentTitle>
-    </>
+      <h1>Welcome to the Camping App</h1>
+      <p>Your adventure starts here!</p>
+      <h2>Explore Our Features</h2>
+      <div>
+        <div>
+          <h3>Discover Campsites</h3>
+          <p>Find the best campsites near you.</p>
+        </div>
+        <div>
+          <h3>Manage Favorites</h3>
+          <p>Save your favorite camps.</p>
+        </div>
+      </div>
+    </section>
   );
 };
 
